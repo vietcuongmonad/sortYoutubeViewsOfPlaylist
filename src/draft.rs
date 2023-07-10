b759a857851d20bf64bce37a40f69b7081711453
vietcuongmonad <hosyvietcuong@gmail.com>
2023-07-11 06:24:13 +0700

    Get video_data; print out title & view count

--- src/draft.rs
+++ src/draft.rs
@@ -1,4 +1,6 @@
-fn main() {
-    println!("Hello, world!");
-}
 
+fn main() {
+    let n = 123456;
+    let formatted = n.to_formatted_string(&Locale::en);
+    println!("{}", formatted);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,78 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(non_snake_case)]
+#![allow(dead_code)]
+
+use std::env;
+use dotenv::dotenv;
+use serde_json::Value;
+
+use futures::{stream, StreamExt, TryStreamExt};
+use num_format::{Locale, ToFormattedString};
+
+async fn get_video_data(
+    playlist_data: &serde_json::Value,
+    developer_key: &str,
+) -> Result<Vec<(String, u64)>, Box<dyn std::error::Error>> {
+    let videos = stream::iter(playlist_data["items"].as_array().unwrap())
+        .then(|item| async move {
+            let title = item["snippet"]["title"].as_str().unwrap().to_string();
+            let video_id = item["snippet"]["resourceId"]["videoId"].as_str().unwrap().to_string();
+            let view_count = get_view_count(video_id, developer_key).await?;
+            Ok::<_, Box<dyn std::error::Error>>((title, view_count))
+        })
+        .try_collect()
+        .await?;
+    Ok(videos)
+}
+
+
+async fn get_view_count(video_id: String, developer_key: &str) -> Result<u64, Box<dyn std::error::Error>> {
+    let url = format!(
+        "https://www.googleapis.com/youtube/v3/videos?id={}&part=statistics&key={}",
+        video_id,
+        developer_key
+    );
+    let response = reqwest::get(&url).await?.text().await?;
+    let video_data: Value = serde_json::from_str(&response)?;
+
+    let view_count = video_data["items"][0]["statistics"]["viewCount"]
+        .as_str()
+        .unwrap_or("0")
+        .parse()
+        .unwrap();
+
+    Ok(view_count)
+}
+
+async fn print_pretty_json(data: &Value) -> Result<(), Box<dyn std::error::Error>> {
+    println!("{}", serde_json::to_string_pretty(&data)?);
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+    let developer_key = env::var("DEVELOPER_KEY").expect("DEVELOPER_KEY must be set");
+
+    let playlist_id = "PL4bD_p5B-nBnHmJCqkdhve5TAoop6I57P";
+    let max_vid = 3;
+
+    let url = format!(
+        "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults={}&playlistId={}&key={}", 
+        max_vid,
+        playlist_id, 
+        developer_key
+    ); 
+
+    let response = reqwest::get(&url).await?.text().await?;
+    let playlist_data: Value = serde_json::from_str(&response)?;
+
+    let videos = get_video_data(&playlist_data, &developer_key).await.unwrap();
+
+    for (title, view_count) in videos {
+        println!("{}: {}", title, view_count.to_formatted_string(&Locale::en));
+    }
+
+    Ok(())
 }
 
 // use reqwest;
